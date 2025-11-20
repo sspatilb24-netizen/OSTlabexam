3135d7d1c04543f47e08e3cd62253cc324e4c045
Shrinivas Patil <sspatil_b24@it.vjti.ac.in>
2025-11-20 13:28:20 +0530

    Add files via upload

---
+++ grades.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+int marks;
+cout<<"Enter your marks"<<endl;
+cin>> marks;
+if(marks>=90 && marks<=100)
+cout<<"Grade:A+"<<endl;
+else if (marks>=80 && marks<90)
+cout<<"Grade:A"<<endl;
+else if (marks>=70 && marks<80)
+cout<<"Grade:B+"<<endl;
+else if (marks>=60 && marks<70)
+cout<<"Grade:B"<<endl;
+else if (marks>=50 && marks<60)
+cout<<"Grade:C+"<<endl;
+else if (marks>=45 && marks<50)
+cout<<"Grade:C"<<endl;
+else if (marks>=35 && marks<40)
+cout<<"Grade:D"<<endl;
+else
+cout<<"Pass"
+}
